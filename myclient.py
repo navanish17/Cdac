3dd89c6b198254f6d696d7e1e2af8b2178ca4dd2
Navanish Pandey <navnishpande17@gmail.com>
2025-09-17 21:41:43 +0530

    update

--- myclient.py
+++
@@ -1,8 +0,0 @@
-import mymodule
-file_path = input("enter file")
-mymodule.count_both(file_path)
-
-import mymodule
-from mymodule import countchar
-file_path = input("enter file")
-mymodule.countchar(file_path)
