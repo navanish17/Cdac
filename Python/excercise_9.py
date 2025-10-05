cf1d5dc6be681acf92277451e62c1636710d84d9
navanish17 <navnishpande17@gmail.com>
2025-10-05 08:30:59 +0530

    update

--- Python/excercise_9.py
+++ Python/excercise_9.py
@@ -12,64 +12,64 @@ conn = pymysql.connect(
 cursor = conn.cursor()
 
 
-# cursor.execute("SELECT * FROM client_master;")
+cursor.execute("SELECT * FROM client_master;")
 
-# rows = cursor.fetchall()
-# for i in rows:
-#     print(i)
-# cursor.close()
-# conn.close()
+rows = cursor.fetchall()
+for i in rows:
+    print(i)
+cursor.close()
+conn.close()
 
-# 3.a: List the names of all clients having ‘a’ as the second letter in their names.
+3.a: List the names of all clients having ‘a’ as the second letter in their names.
 
-# cursor.execute("select name1 from client_master where name1 like '_a%';")
-# for i in cursor.fetchall():
-#     print(i)
-# cursor.close()
-# conn.close()
+cursor.execute("select name1 from client_master where name1 like '_a%';")
+for i in cursor.fetchall():
+    print(i)
+cursor.close()
+conn.close()
 
-# b. List the clients who stay in a city whose first letter is ‘M’.
-# cursor.execute("SELECT NAME1 FROM CLIENT_MASTER WHERE CITY LIKE 'M%';")
+b. List the clients who stay in a city whose first letter is ‘M’.
+cursor.execute("SELECT NAME1 FROM CLIENT_MASTER WHERE CITY LIKE 'M%';")
 
-# for i in cursor.fetchall():
-#     print(i)
-# cursor.close()
-# conn.close()
+for i in cursor.fetchall():
+    print(i)
+cursor.close()
+conn.close()
 
-# c. List all clients who stay in ‘Bangalore’ or ‘Mangalore’.
-# cursor.execute("SELECT NAME1 FROM CLIENT_MASTER WHERE CITY in ('Banglore', 'Manglore');")
-# for i in cursor.fetchall():
-#     print(i)
-# cursor.close()
-# conn.close()
+c. List all clients who stay in ‘Bangalore’ or ‘Mangalore’.
+cursor.execute("SELECT NAME1 FROM CLIENT_MASTER WHERE CITY in ('Banglore', 'Manglore');")
+for i in cursor.fetchall():
+    print(i)
+cursor.close()
+conn.close()
 
-# d. List all clients whose BalDue is greater than value 10000.
-# cursor.execute("SELECT NAME1 FROM CLIENT_MASTER WHERE CITY in ('Banglore', 'Manglore');")
-# for i in cursor.fetchall():
-#     print(i)
-# cursor.close()
-# conn.close()
+d. List all clients whose BalDue is greater than value 10000.
+cursor.execute("SELECT NAME1 FROM CLIENT_MASTER WHERE CITY in ('Banglore', 'Manglore');")
+for i in cursor.fetchall():
+    print(i)
+cursor.close()
+conn.close()
 
-# e. List all information from the Sales_order table for order placed in the month of june
-# cursor.execute("SELECT * FROM SALES_ORDER WHERE ORDERDATE between ('2002-06-1') and ('2002-06-30');")
-# for i in cursor.fetchall():
-#     print(i)
-# cursor.close()
-# conn.close()
+e. List all information from the Sales_order table for order placed in the month of june
+cursor.execute("SELECT * FROM SALES_ORDER WHERE ORDERDATE between ('2002-06-1') and ('2002-06-30');")
+for i in cursor.fetchall():
+    print(i)
+cursor.close()
+conn.close()
 
-# f. List the Order No & day on which clients placed their order.
-# cursor.execute("SELECT ORDERNO, ORDERDATE FROM SALES_ORDER;")
-# for i in cursor.fetchall():
-#     print(i)
-# cursor.close()
-# conn.close()
+f. List the Order No & day on which clients placed their order.
+cursor.execute("SELECT ORDERNO, ORDERDATE FROM SALES_ORDER;")
+for i in cursor.fetchall():
+    print(i)
+cursor.close()
+conn.close()
 
-# g. List the names, city and state of clients who are not in the state of ‘Maharashtra’.
-# cursor.execute("SELECT name1,city,state FROM client_master where state != 'Maharashtra';")
-# for i in cursor.fetchall():
-#     print(i)
-# cursor.close()
-# conn.close()
+g. List the names, city and state of clients who are not in the state of ‘Maharashtra’.
+cursor.execute("SELECT name1,city,state FROM client_master where state != 'Maharashtra';")
+for i in cursor.fetchall():
+    print(i)
+cursor.close()
+conn.close()
 
 4. Exercises on Using Having, Group By and Joins in Python IDLE:
 a. Printing the description and total quantity sold for each product.
